b0fabcd038eb2477558fe7b66aaa0e5e7b28e439
Mike C. Fletcher <mcfletch@vrplumber.com>
2017-05-12 22:47:15 -0400

    Remove the broken absolute import introduced during packaging

--- sphfile/__init__.py
+++ sphfile/__init__.py
@@ -1,6 +1,4 @@
 """sphfile - Numpy-based NIST SPH audio-file reader"""
-from __future__ import absolute_imports
-
 __version__ = '1.0.0'
 __author__ = 'Mike C. Fletcher <mcfletch@vrplumber.com>'
 __all__ = ['SPHFile']
