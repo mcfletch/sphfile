0e5f33d3082ded092aeb7568eef62f68ab376b45
Mike C. Fletcher <mcfletch@vrplumber.com>
2018-09-18 23:40:21 -0400

    Bump version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sphfile",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/mcfletch/sphfile",
 
     author="Mike C. Fletcher",
